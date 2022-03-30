29a120904f0d341490330e176f047c4ce0438e11
Mark Li <itmarkibfb@gmail.com>
2022-03-30 14:02:58 +0800

    update

--- js/other.js
+++ js/other.js
@@ -45,13 +45,15 @@ const POPUP_GROUPS = {
             <button class="btn" style="font-family: 'Andy Bold';" onclick="player.options.font = 'Andy Bold'">Andy Bold</button>
             <button class="btn" style="font-family: Arial, Helvetica, sans-ser;" onclick="player.options.font = 'Arial, Helvetica, sans-ser'">Arial</button>
             <button class="btn" style="font-family: Bahnschrift;" onclick="player.options.font = 'Bahnschrift'">Bahnschrift</button>
-            <button class="btn" style="font-family: Chiron Hei HK;" onclick="player.options.font = 'Chiron Hei HK'">Chiron Hei HK/昭源黑體</button>
             <button class="btn" style="font-family: Comic Sans MS;" onclick="player.options.font = 'Comic Sans MS'">Comic Sans MS</button>
             <button class="btn" style="font-family: Courier;" onclick="player.options.font = 'Courier'">Courier</button>
             <button class="btn" style="font-family: Cousine;" onclick="player.options.font = 'Cousine'">Cousine</button>
             <button class="btn" style="font-family: 'Flexi IBM VGA False';" onclick="player.options.font = 'Flexi IBM VGA False'">Flexi IBM VGA False</button>
+            <button class="btn" style="font-family: GenYoGothic TW;" onclick="player.options.font = 'GenYoGothic TW'">源樣黑體 TW</button>
+            <button class="btn" style="font-family: GenYoGothic JP;" onclick="player.options.font = 'GenYoGothic JP'">源樣黑體 JP</button>
             <button class="btn" style="font-family: Inconsolata;" onclick="player.options.font = 'Inconsolata'">Inconsolata</button>
             <button class="btn" style="font-family: 'Lucida Handwriting';" onclick="player.options.font = 'Lucida Handwriting'">Lucida Handwriting</button>
+            <button class="btn" style="font-family: 'LXGWWenKai';" onclick="player.options.font = 'LXGWWenKai'">LXGWWenKai/霞鶩文楷</button>
             <button class="btn" style="font-family: Monospace-Typewritter;" onclick="player.options.font = 'Monospace-Typewritter'">Monospace Typewritter</button>
 			<button class="btn" style="font-family: 'MS Sans Serif';" onclick="player.options.font = 'MS Sans Serif'">MS Sans Serif</button>
             <button class="btn" style="font-family: 'Noto Sans SC';" onclick="player.options.font = 'Noto Sans SC'">Noto Sans SC</button>
